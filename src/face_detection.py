9d6c0ae822d6e5e96014393b432eb37ed6c2b5d5
Abdulrehmanghani <amaizingboy197@gmail.com>
2021-06-18 10:32:36 +0500

    A new code feature add
    A face detection model has been loaded in this file.
    pree and post processing done
    model output send to the main file
    
    testing left

---
+++ src/face_detection.py
@@ -0,0 +1,84 @@
+'''
+This is a sample class for a model. You may choose to use it as-is or make any changes to it.
+This has been provided just to give you an idea of how to structure your model class.
+'''
+
+class FaceDetection:
+    '''
+    Class for the Face Detection Model.
+    '''
+    def __init__(self, model_name, device='CPU', extensions=None):
+        '''
+        TODO: Use this to set your instance variables.
+        '''
+        self.model_weights=model_name+'.bin'
+        self.model_structure=model_name+'.xml'
+        self.device=device
+        self.threshold=threshold
+
+        try:
+            self.core = IECore()
+            self.model=core.read_network(model=model_structure, weights=model_weights)
+        except Exception as e:
+            raise ValueError("Could not Initialise the network. Have you enterred the correct model path?")
+
+        self.input_name=next(iter(self.model.inputs))
+        self.input_shape=self.model.inputs[self.input_name].shape
+        self.output_name=next(iter(self.model.outputs))
+        self.output_shape=self.model.outputs[self.output_name].shape
+
+    def load_model(self):
+        '''
+        TODO: You will need to complete this method.
+        This method is for loading the model to the device specified by the user.
+        If your model requires any Plugins, this is where you can load them.
+        '''
+        self.net = self.core.load_network(network= self.model, device_name=self.device, num_requests=1)
+
+    def predict(self, image):
+        '''
+        TODO: You will need to complete this method.
+        This method is meant for running predictions on the input image.
+        '''
+        input_img_dict = self.preprocess_input(self, image):
+        output = net.infer(input_img_dict)
+        result = self.output[self.output_name]
+        cords =preprocess_outputs(result,image)
+        faces_coordinates = faces_coordinates.astype(np.int32)
+        cropped_face_image = image[faces_coordinates[1]:faces_coordinates[3], faces_coordinates[0]:faces_coordinates[2]]
+        return cropped_face_image, faces_coordinates
+
+    def check_model(self):
+        supported_layers = self.core.query_network(network=self.network, device_name=self.device)
+        unsupported_layers = [layer for layer in self.network.layers.keys() if layer not in supported_layers]
+        if len(unsupported_layers) > 0:
+            print("Check extention of these unsupported layers =>" + str(unsupported_layers))
+            exit(1)
+        print("All layers are supported")
+
+    def preprocess_input(self, image):
+    '''
+    Before feeding the data into the model for inference,
+    you might have to preprocess it. This function is where you can do that.
+    ''' input_img=cv2.resize(image, (self.input_shape[3],self.input_shape[2]))
+        input_img=input_img.transpose((2, 0, 1))
+        input_img = input_img.reshape(1, *input_img.shape)
+        input_dict{self.input_name: input_img}
+        
+        return input_dict
+
+    def preprocess_output(self, outputs, image):
+    '''
+    Before feeding the output of this model to the next model,
+    you might have to preprocess the output. This function is where you can do that.
+    '''
+        cords = []
+        for box in outputs[0][0]: # Output shape is 1x1x100x7
+            conf = box[2]
+            if conf >= self.threshold:
+                x1 = int(coords[3] * image.shape[1])
+                y1 = int(coords[4] * image.shape[0])
+                x2 = int(coords[5] * image.shape[1])
+                y2 = int(coords[6] * image.shape[0])
+                cords.append((x1,y1,x2,y2))
+        return cords
