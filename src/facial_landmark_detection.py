291c0160aeea3c956678bbff59b6cdd279b9087e
Abdulrehmanghani <amaizingboy197@gmail.com>
2021-06-18 11:21:21 +0500

    Update facial land mark file updated model file deleted

--- src/facial_landmark_detection.py
+++ src/facial_landmark_detection.py
@@ -3,7 +3,7 @@ This is a sample class for a model. You may choose to use it as-is or make any c
 This has been provided just to give you an idea of how to structure your model class.
 '''
 
-class Model_X:
+class FacialLandMark:
     '''
     Class for the Face Detection Model.
     '''
@@ -11,7 +11,21 @@ class Model_X:
         '''
         TODO: Use this to set your instance variables.
         '''
-        raise NotImplementedError
+        self.model_weights=model_name+'.bin'
+        self.model_structure=model_name+'.xml'
+        self.device=device
+        self.threshold=threshold
+
+        try:
+            self.core = IECore()
+            self.model=core.read_network(model=model_structure, weights=model_weights)
+        except Exception as e:
+            raise ValueError("Could not Initialise the network. Have you enterred the correct model path?")
+
+        self.input_name=next(iter(self.model.inputs))
+        self.input_shape=self.model.inputs[self.input_name].shape
+        self.output_name=next(iter(self.model.outputs))
+        self.output_shape=self.model.outputs[self.output_name].shape
 
     def load_model(self):
         '''
@@ -26,17 +40,33 @@ class Model_X:
         TODO: You will need to complete this method.
         This method is meant for running predictions on the input image.
         '''
-        raise NotImplementedError
+        input_img_dict = self.preprocess_input(self, image):
+        output = net.infer(input_img_dict)
+        result = self.output[self.output_name]
+        cords =preprocess_outputs(result,image)
+        cords = cords.astype(np.int32)
+        face_image = image[cords[1]:cords[3], cords[0]:cords[2]]
+        return face_image, cords
 
     def check_model(self):
-        raise NotImplementedError
+        supported_layers = self.core.query_network(network=self.network, device_name=self.device)
+        unsupported_layers = [layer for layer in self.network.layers.keys() if layer not in supported_layers]
+        if len(unsupported_layers) > 0:
+            print("Check extention of these unsupported layers =>" + str(unsupported_layers))
+            exit(1)
+        print("All layers are supported")
 
     def preprocess_input(self, image):
     '''
     Before feeding the data into the model for inference,
     you might have to preprocess it. This function is where you can do that.
     '''
-        raise NotImplementedError
+        input_img=cv2.resize(image, (self.input_shape[3],self.input_shape[2]))
+        input_img=input_img.transpose((2, 0, 1))
+        input_img = input_img.reshape(1, *input_img.shape)
+        input_dict{self.input_name: input_img}
+        
+        return input_dict
 
     def preprocess_output(self, outputs):
     '''
